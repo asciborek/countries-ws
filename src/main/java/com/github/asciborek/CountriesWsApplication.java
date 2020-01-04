7667199de3591099271d93b22c2e46587b73f382
Aleksander Sciborek <aleksandersciborek@gmail.com>
2020-01-04 20:55:58 +0100

    init commit

---
+++ src/main/java/com/github/asciborek/CountriesWsApplication.java
@@ -0,0 +1,13 @@
+package com.github.asciborek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/asciborek/CountriesWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.asciborek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
